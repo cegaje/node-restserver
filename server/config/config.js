64fb453e2925f05fbbf0a660d5acd9c0c4e6203b
Cegaje <Cegaje17@gmail.com>
2020-10-07 02:05:31 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:FB517xF8kN2PdMIS@cluster0.qtfnv.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
